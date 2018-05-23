9e61e379db4cbe615e674bf3331f948644630b4f
Shubham Dash <shubham.ddash@gmail.com>
2018-05-22 22:39:55 -0400

    Variational autoencoder with varying architectures

--- RNN_VAE.py
+++ RNN_VAE.py
@@ -1,3 +1,6 @@
+''' Python implementation for Variational Autoencoders 
+SDash, May 2018
+'''
 import matplotlib.pyplot as plt
 import numpy as np
 import tensorflow as tf
@@ -12,12 +15,12 @@ from keras.datasets import mnist
 from keras.utils import plot_model
 from keras import backend as K
 import matplotlib.pyplot as plt
-import argparse
 import os
 
-# parameters
+# parameters for the model
 intermediate_dim = 1024
 latent_dim = 45
+alpha = 1.
 
 # reparameterization in isotropic Gaussian 
 def sample_z(args):
@@ -31,20 +34,35 @@ def sample_z(args):
 
 class Model():
 	
-	def load_proc_data():
+	def __init__(self, image_size, alpha, intermediate_dim, latent_dim):
+		self.org_dim = image_size * image_size
+		self.alpha = alpha
+		self.intermediate_dim = intermediate_dim
+		self.latent_dim = latent_dim
+
+	# load and process the MNIST data
+	def load_proc_data(self, org_dim):
 		(x_tr, y_tr), (x_ts, y_ts) = mnist.load_data()
 		image_size = x_tr.shape[1]
-		org_dim = image_size * image_size
 		x_tr = np.reshape(x_tr, [-1, org_dim]).astype('float32') / 255
 		x_ts = np.reshape(x_ts, [-1, org_dim]).astype('float32') / 255
 		return (x_tr, y_tr), (x_ts, y_ts)
 
-	def build_vae(input_shape, output_shape):
+	def Loss(self, z_log_var, z_mean, x, x_decoded_mean):
+		kl_loss = 1. + z_log_var - K.square(z_mean) - K.exp(z_log_var)
+		kl_loss = -0.5 * K.sum(kl_loss, axis=-1)
+		recon_loss = self.alpha * self.org_dim * objectives.categorical_crossentropy(x, x_decoded_mean)
+
+
+	# build the Variational Autoencoders
+	def build_vae(self, input_shape, output_shape):
 		inp = Input(shape=(input_shape,), activation='relu')
-		x = Dense(intermediate_dim, activation='relu')(inp)
-		z_mean = Dense(latent_dim, name='z_mean')(x)
-		z_log_var = Dense(latent_dim, activation='relu')(x)
+		x = Dense(self.intermediate_dim, activation='relu')(inp)
+		z_mean = Dense(self.latent_dim, name='z_mean')(x)
+		z_log_var = Dense(self.latent_dim, activation='relu')(x)
 		z = Lambda(sample_z, output_shape=(latent_dim,))([z_mean, z_log_var])
+
+		# general encoder-decoder architecture subject to change
 
 		# encoder
 		encoder = Model(inp, [z_mean, z_log_var])
@@ -54,4 +72,23 @@ class Model():
 		latent_input = Input(shape=(latent_dim,))
 		x = Dense(intermediate_dim, activation='relu')(latent_input)
 		outputs = Dense(org_dim, activation='sigmoid')(x)
+		decoder = Model(latent_input, outputs)
+		decoder.summary()
 
+		outputs = decoder(encoder(inp)[2])
+		vae = Input(inp, outputs)
+		return vae
+
+# main function
+if __name__ == "__main__":
+
+	# instatiate the Class and load the data
+	model = Model(img_dim, alpha, intermediate_dim, latent_dim)
+	x_tr, y_tr, x_ts, y_ts = model.load_proc_data()
+	
+	# building the variational autoencoder and fitting the model
+	vae = build_vae(org_dim, org_dim)
+	vae.fit(x_tr, y_tr)
+
+	# predictions on the test data
+	y_pred = vae.predict(x_ts)
