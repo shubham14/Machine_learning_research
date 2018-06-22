c899b33bd71abfac4b117b7f2e6009858443c343
Shubham Dash <shubham.ddash@gmail.com>
2018-06-21 21:17:52 -0400

    InfoGAN in progress

--- RNN_VAE.py
+++ RNN_VAE.py
@@ -1,4 +1,4 @@
-''' Python implementation for Variational Autoencoders 
+''' Python implementation for Variational Autoencoders with CNN
 SDash, May 2018
 '''
 import matplotlib.pyplot as plt
@@ -12,15 +12,19 @@ from __future__ import division
 from __future__ import print_function
 from keras.models import Model
 from keras.datasets import mnist
+from keras.layers.convolutional import Conv1D, Conv2D, Conv2DTranspose
+from keras.layers.convolutional import MaxPooling1D, MaxPooling2D, UpSampling1D, UpSampling2D
 from keras.utils import plot_model
 from keras import backend as K
 import matplotlib.pyplot as plt
 import os
+import sys
 
 # parameters for the model
 intermediate_dim = 1024
 latent_dim = 45
 alpha = 1.
+n_filters = (64, 64, 64)
 
 # reparameterization in isotropic Gaussian 
 def sample_z(args):
@@ -54,30 +58,66 @@ class Model():
 		recon_loss = self.alpha * self.org_dim * objectives.categorical_crossentropy(x, x_decoded_mean)
 
 
+	# general encoder-decoder architecture is subject to change
 	# build the Variational Autoencoders
 	def build_vae(self, input_shape, output_shape):
+
 		inp = Input(shape=(input_shape,), activation='relu')
-		x = Dense(self.intermediate_dim, activation='relu')(inp)
+		# x = Dense(self.intermediate_dim, activation='relu')(inp)
+		x = Conv2D(n_filters[0], (3,3), padding='same')(inp)
+		x = MaxPooling2D((2,2), padding='same')(x)
+		x = Conv2D(n_filters[1], (3,3), padding='same')(x)
+		x = MaxPooling2D((2,2), padding='same')(x)
+		x = Conv2D(n_filters[0], (3,3), padding='same')(x)
+		x = MaxPooling2D((2,2), padding='same')(x)
 		z_mean = Dense(self.latent_dim, name='z_mean')(x)
 		z_log_var = Dense(self.latent_dim, activation='relu')(x)
 		z = Lambda(sample_z, output_shape=(latent_dim,))([z_mean, z_log_var])
-
-		# general encoder-decoder architecture subject to change
 
 		# encoder
 		encoder = Model(inp, [z_mean, z_log_var])
 		encoder.summary()
 
 		# decoder
-		latent_input = Input(shape=(latent_dim,))
-		x = Dense(intermediate_dim, activation='relu')(latent_input)
-		outputs = Dense(org_dim, activation='sigmoid')(x)
-		decoder = Model(latent_input, outputs)
-		decoder.summary()
+		# latent_input = Input(shape=(latent_dim,))
+		# x = Dense(intermediate_dim, activation='relu')(latent_input)
+		# outputs = Dense(org_dim, activation='sigmoid')(x)
+		# decoder = Model(latent_input, outputs)
+		# decoder.summary()
 
-		outputs = decoder(encoder(inp)[2])
-		vae = Input(inp, outputs)
+		decoder_hidden = Dense(intermediate_dim, activation='relu')
+		decoder_expand = Dense(self.org_dim, activation='relu')
+		decoder_reshape = Reshape(self.org_dim)
+		decoder_deconv1 = Conv2DTranspose(n_filters[2],
+										  kernel_size=(3,3),
+										  padding='same',
+										  strides=2,
+										  activation='relu')
+		decoder_deconv1 = Conv2DTranspose(n_filters[1],
+								  kernel_size=(3,3),
+								  padding='same',
+								  strides=2,
+								  activation='relu')
+		decoder_deconv1 = Conv2DTranspose(n_filters[0],
+								  kernel_size=(3,3),
+								  padding='same',
+								  strides=2,
+								  activation='relu')	
+		decoder_conv4 = Conv2D(3, kernel_size=(3,3), padding='same', activation='sigmoid')
+		x = decoder_hidden(z)
+		x = decoder_expand(x)
+		x = decoder_reshape(x)
+		x = decoder_deconv1(x)
+		x = decoder_deconv2(x)
+		x = deocder_decoder3(x)
+		outputs= decoder_conv4(x)
+		
+		vae = Model(inp, outputs)
+		loss = Loss(z_log_var, z_mean, K.flatten(inp), K.flatten(outputs))
+		vae.add_loss(loss)
+		vae.compile(optimizer='adam', loss=None, metrics=['mse'])
 		return vae
+
 
 # main function
 if __name__ == "__main__":
