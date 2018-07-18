38b6ccd5336cf2f1ec0fd1a5a871d1c926192d7a
Shubham Dash <shubham.ddash@gmail.com>
2018-07-18 14:16:15 -0400

    Text feature Extraction

--- TextToImage/Text_feature_extraction.py
+++ TextToImage/Text_feature_extraction.py
@@ -1,121 +1,63 @@
 '''
-Python implemetation of encoded text features
+Python implemetation of extracting text features using Bi_LSTM
+SDash Jul 18
 '''
 
-import tensorflow as tf
-from tensorflow.contrib import rnn
-import numpy as np
+import torch
+import torch.nn as nn
 import os
 import sys
-from nltk import *
-from sklearn import *
-import re
-from utils import get_init_embedding
+import nltk
 
-class Model:
-    def __init__(self, reversed_dict, article_max_len, summary_max_len,
-                 args, forward_only=False, batch_size):
-        self.vocabulary_size = len(reversed_dict)
-        self.embedding_size = args.embedding_size
-        self.num_hidden = args.num_hidden
-        self.num_layers = args.num_layers
-        self.learning_rate = args.beam_width
-        if not forward_only:
-            self.keep_prob = args.keep_prob
+class BaseRNN(nn.Module):
+    
+    SYM_MASK = "MASK"
+    SYM_EOS = "EOS"
+    
+    def __init__(self, vocab_size, max_len, hidden_size, input_dropout_p, dropout_p,
+                 n_layers, rnn_cell):
+        super(BaseRNN, self).__init__()
+        self.vocab_size = vocab_size
+        self.max_len = max_len
+        self.hidden_size = hidden_size
+        self.n_layers = n_layers
+        self.input_dropout_p = input_dropout_p
+        self.dropout_p = dropout_p
+        self.input_dropout = nn.Dropout(p=input_dropout_p)
+        if rnn_cell.lower() == 'lstm':
+            self.rnn_cell = nn.LSTM
+        elif rnn_cell == 'gru':
+            self.rnn_cell = nn.GRU
         else:
-            self.keep_prob = 1.0
-        self.cell = tf.nn.rnn_cell.BasicsLSTM
-        with tf.variable_scope("decoder/projection"):
-            self.projection_layer = tf.layers.Dense(self.vocabulary_size, 
-                                                    use_bias=False)
-        self.batch_size = batch_size
-        self.X = tf.placeholder(tf.int32, [None, article_max_len])
-        self.X_len = tf.placeholder(tf.int32, [None])
-        self.decoder_input = tf.placeholder(tf.int32, [None, summary_max_len])
-        self.deocder_len = tf.placeholder(tf.int32, [None])
-        self.decoder_target = tf.placeholder(tf.int32, [None, summary_max_len])
-        self.global_step = tf.Variable(0, trainable=False)
+            raise ValueError("Unsupported RNN cell")
+            
+    def forward(self, *args, **kwargs):
+        raise NotImplementedError()
         
-        with tf.name_scope("embedding"):
-            if not forward_only and args.glove:
-                init_embeddings = tf.constant(get_init_embedding(reversed_dict,
-                                                                 self.embedding_size),
-                                                dtype=tf.float32)
-            else:
-                init_embeddings = tf.random_uniform([self.vocabulary_size,
-                                                     self.embedding_size],
-                                                    -1.0, 1.0)
-            self.embeddings = tf.get_variable("embeddings", initializer=init_embeddings)
-            self.encoder_emb_inp = tf.transpose(tf.nn.embedding_lookup(self.embeddings,
-                                                                       self.X), perm=[1, 0, 2])
-            self.decoder_emb_inp = tf.transpose(tf.nn.embedding_lookup(self.embeddings,
-                                                                       self.decoder_input), perm=[1, 0, 2])
-            with tf.name_scope("encoder"):
-                fw_cells = [self.cell(self.num_hidden) for _ in range(self.num_layers)]
-                bw_cells = [self.cell(self.num_hidden) for _ in range(self.num_layers)]
-                fw_cells = [rnn.DropoutWrapper(cell) for cell in fw_cells]
-                bw_cells = [rnn.DropoutWrapper(cell) for cell in bw_cells]
+class EncoderRNN(BaseRNN):
+    def __init__(self, vocab_size, max_len, hidden_size, input_dropout_p=0,
+                 dropout_p=0, n_layers=1, bidirctional=True,
+                 rnn_cell='lstm', variable_lengths=False, embedding=None, 
+                 update_embedding=True):
+        super(EncoderRNN, self).__init__(vocab_size, max_len, hidden_size,
+             input_dropout_p, dropout_p, n_layers, rnn_cell)
+        self.variable_lengths = variable_lengths
+        self.embedding = nn.Embedding(vocab_size, hidden_size)
+        if embedding is not None:
+            self.embedding_weight = nn.Parameter(embedding)
+        self.embedding.weight.requires_grad = update_embedding
+        self.rnn = self.rnn_cell(hidden_size, hidden_size, n_layers, batch_first=True,
+                                 bidirectional=bidirectional, dropout=dropout_p)
+        
+    def forward(self, input_var, input_length=None):
+        embedded = self.embedding(input_var)
+        embedded = self.input_dropout(embedded)
+        if self.variable_lengths:
+            embedded = nn.utils.rnn.pack_padded_sequence(embedded, input_lengths, batch_first=True)
+        output, hidden = self.rnn(embedded)
+        if self.variable_lengths:
+            output, _ = nn.utils.rnn.pack_padded_sequence(output, batch_first=True)
+        global_sent_vector = torch.cat([b(x) for b in hidden], 1)
+        return output, hidden, global_sent_vector
+    
                 
-                encoder_outputs, encoder_state_fwm encoder_state_bw = tf.contrib.rnn.stack_bidirectional_dynamic_rnn(
-                                        fw_cells, bw_cells, self.encoder_emb_inp,
-                                        sequence_length=self.X_len, time_major=True, dtype=tf.float32)
-                self.encoder_output = tf.concat(encoder_outputs, 2)
-                encoder_state_c = tf.concat((encoder_state_fw[0].c, encoder_state_bw[0].c), 1)
-                encoder_state_h = tf.cocnat((encoder_state_fw[0].h, encoder_state_bw[0].h), 1)
-                self.encoder_state = rnn.LSTMStateTuple(c=encoder_state_c,
-                                                        h=encoder_state_h)
-            with tf.name_scope("decoder"), tf.variable_scope("decoder") as decoder_scope:
-                decoder_cell = self.cell(self.num_hidden * 2)
-                
-                if not forward_only:
-                    attention_states = tf.transpose(self.encoder_output. [1, 0, 2])
-                    attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(
-                            self.num_hidden * 2, attention_states, memeory_sequence_length=self.X_len,
-                            normalize=True)
-                    decoder_cell = tf.contrib.seq2seq.AttentionWrapper(decoder_cell, attention_mechanism,
-                                                                       attention_layer_size=self.num_hidden)
-                    initial_state = decoder_cell.zero_state(dtype=tf.float32, batch_size=self.batch_size)
-                                    initial_state = initial_state.clone(cell_state=self.encoder_state)
-                    helper = tf.contrib.seq2seq.TrainingHelper(self.decoder_emb_inp, self.decoder_len, time_major=True)
-                    decoder = tf.contrib.seq2seq.BasicDecoder(decoder_cell, helper, initial_state)
-                    outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(decoder, output_time_major=True, scope=decoder_scope)
-                    self.decoder_output = outputs.rnn_output
-                                    self.logits = tf.transpose(
-                        self.projection_layer(self.decoder_output), perm=[1, 0, 2])
-                    self.logits_reshape = tf.concat(
-                        [self.logits, tf.zeros([self.batch_size, summary_max_len - tf.shape(self.logits)[1], self.vocabulary_size])], axis=1)
-                            else:
-                tiled_encoder_output = tf.contrib.seq2seq.tile_batch(
-                    tf.transpose(self.encoder_output, perm=[1, 0, 2]), multiplier=self.beam_width)
-                tiled_encoder_final_state = tf.contrib.seq2seq.tile_batch(self.encoder_state, multiplier=self.beam_width)
-                tiled_seq_len = tf.contrib.seq2seq.tile_batch(self.X_len, multiplier=self.beam_width)
-                attention_mechanism = tf.contrib.seq2seq.BahdanauAttention(
-                    self.num_hidden * 2, tiled_encoder_output, memory_sequence_length=tiled_seq_len, normalize=True)
-                decoder_cell = tf.contrib.seq2seq.AttentionWrapper(decoder_cell, attention_mechanism,
-                                                                   attention_layer_size=self.num_hidden * 2)
-                initial_state = decoder_cell.zero_state(dtype=tf.float32, batch_size=self.batch_size * self.beam_width)
-                initial_state = initial_state.clone(cell_state=tiled_encoder_final_state)
-                decoder = tf.contrib.seq2seq.BeamSearchDecoder(
-                    cell=decoder_cell,
-                    embedding=self.embeddings,
-                    start_tokens=tf.fill([self.batch_size], tf.constant(2)),
-                    end_token=tf.constant(3),
-                    initial_state=initial_state,
-                    beam_width=self.beam_width,
-                    output_layer=self.projection_layer
-                )
-                outputs, _, _ = tf.contrib.seq2seq.dynamic_decode(
-                    decoder, output_time_major=True, maximum_iterations=summary_max_len, scope=decoder_scope)
-                self.prediction = tf.transpose(outputs.predicted_ids, perm=[1, 2, 0])
-
-        with tf.name_scope("loss"):
-            if not forward_only:
-                crossent = tf.nn.sparse_softmax_cross_entropy_with_logits(
-                    logits=self.logits_reshape, labels=self.decoder_target)
-                weights = tf.sequence_mask(self.decoder_len, summary_max_len, dtype=tf.float32)
-                self.loss = tf.reduce_sum(crossent * weights / tf.to_float(self.batch_size))
-
-                params = tf.trainable_variables()
-                gradients = tf.gradients(self.loss, params)
-                clipped_gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
-                optimizer = tf.train.AdamOptimizer(self.learning_rate)
