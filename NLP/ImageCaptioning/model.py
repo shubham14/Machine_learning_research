e392bccc8d647724db4e67cb6315b4141000ca1f
Shubham Dash <shubham.ddash@gmail.com>
2019-10-24 23:03:09 -0700

    Added Model definitions

--- NLP/ImageCaptioning/model.py
+++ NLP/ImageCaptioning/model.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 from tqdm import tqdm
+import tensorflow.keras.layers as layers
 
 def extractInceptionV3Features():
     image_model = tf.keras.applications.InceptionV3(include_top=False,
@@ -9,4 +10,55 @@ def extractInceptionV3Features():
     hidden_layer = image_model.layers[-1].output
 
     image_features_extract_model = tf.keras.Model(new_input, hidden_layer)
+
+class BahdanauAttention(tf.keras.Model):
+    def __init__(self, units):
+        super(BahdanauAttention, self).__init__()
+        self.W1 = tf.keras.layers.Dense(units)
+        self.W2 = tf.keras.layers.Dense()
+        self.V = layers.Dense(1)
     
+    def call(self, features, hidden):
+        hidden_with_time_axis = tf.expand_dims(hidden, 1)
+        score = tf.nn.tanh(self.W1(features) + self.W2(hidden_with_time_axis))
+        attention_weights = tf.nn.softmax(self.V(score), axis=1)
+        context_vec = attention_weights * features
+        context_vec = tf.reduce_sum(context_vec, axis=1)
+        return context_vec, attention_weights
+
+
+class CNN_encoder(tf.keras.Model):
+    def __init__(self, embedding_dim):
+        super(CNN_encoder, self).__init__()
+        self.fc = layers.Dense(embedding_dim)
+    
+    def call(self, x):
+        x = self.fc(x)
+        x = tf.nn.relu(x)
+        return x 
+
+class RNN_Decoder(tf.keras.Model):
+    def __init__(self, embedding_dim, units, vocab_size):
+        super(RNN_Decoder, self).__init__()
+        self.units = units
+        self.embedding = tf.keras.layers.Embedding(vocab_size, embedding_dim)
+        self.gru = tf.keras.layers.GRU(self.units,
+                                        return_sequence=True,
+                                        return_state=True,
+                                        recurrent_initializer='glorot_uniform')
+        self.fc1 = tf.keras.layers.Dense(self.units)
+        self.fc2 = tf.keras.layers.Dense(vocab_size)
+        self.attention = BahdanauAttention(self.units)
+
+    def call(self, x, features, hidden):
+        context_vec, attention_weights = self.attention(features, hidden)
+        x = self.embedding(x)
+        x = tf.concat([tf.expand_dims(context_vec, 1), x], axis=-1)
+        output, state = self.gru(x)
+        x = self.fc1(output)
+        x = tf.reshape(x, (-1, x.shape[2]))
+        x = self.fc2(x)
+        return x, state, attention_weights
+    
+    def reset_weights(self, batch_size):
+        return tf.zeros((batch_size, self.units))
